18c3cf02e75bc5fb3fb0be38b1bc4284a2bec0c9
MrGonzalezCruz <mrgonzalezcruz@gmail.com>
2018-01-05 09:31:58 -0600

    :v tupla

--- SIGECU/src/main/java/com/sigecu/service/AlumnoHasEventoService.java
+++ SIGECU/src/main/java/com/sigecu/service/AlumnoHasEventoService.java
@@ -1,9 +1,12 @@
 package com.sigecu.service;
 
 import com.sigecu.entity.AlumnoHasEvento;
+import com.sigecu.entity.Alumno_Has_Eventos;
 
 public interface AlumnoHasEventoService {
 
 	public abstract AlumnoHasEvento findAlumnoHasEventoById(int idAHE);
+	
+	public abstract void findAlumnoHasEventoByIdAlumnoAndIdEvento(int id_alumno, int id_evento);
 	
 }
