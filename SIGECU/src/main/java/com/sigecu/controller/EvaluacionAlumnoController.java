1c159dccde353ba0a6269eda2aa66b79348451de
RolandoCM <castillor493@gmail.com>
2018-01-10 12:46:30 -0600

    alm

--- SIGECU/src/main/java/com/sigecu/controller/EvaluacionAlumnoController.java
+++ SIGECU/src/main/java/com/sigecu/controller/EvaluacionAlumnoController.java
@@ -4,6 +4,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sigecu.constant.ViewConstant;
+import com.sigecu.model.AlumnoModel;
 import com.sigecu.model.EvaluacionesModel;
 import com.sigecu.model.PreguntasModel;
 import com.sigecu.model.RespuestasModel;
 import com.sigecu.service.AdminExamService;
+import com.sigecu.service.DefineUsuarioService;
 import com.sigecu.service.EvaluacionAlumnoService;
 
 
@@ -25,22 +30,31 @@ import com.sigecu.service.EvaluacionAlumnoService;
 
 
 @Controller
+@PreAuthorize("hasRole('ROLE_ALUMNO')")
 @RequestMapping("/EvaluacionAlumno")
 public class EvaluacionAlumnoController {
 	
 	private static final Log LOG = LogFactory.getLog(EvaluacionAlumnoController.class);
-
+	private User user;
+	AlumnoModel alumnoModel;
 	
 	
 	@Autowired
 	@Qualifier("EvaluacionAlumnoImpl")
 	private EvaluacionAlumnoService evaluacionAlumnoService;
+	@Autowired
+	@Qualifier("defineUsuario")
+	private DefineUsuarioService defineUsuario;
 
 	@GetMapping("/mostrarExamen")
-	public ModelAndView mostrarExamenN(Model model) {
-		int idEvaluacion=3;
+	public ModelAndView mostrarExamenN(@RequestParam(name="idEvaluacion", required=false)int idEvaluacion, Model model) {
 		PreguntasModel preModel = new PreguntasModel();
 		RespuestasModel respuestasModel = new RespuestasModel();
+		user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		alumnoModel =defineUsuario.buscarUsuarioAlumno(user.getUsername());
+		
+		
+		
 		ModelAndView mav = new ModelAndView(ViewConstant.MOSTRAR_EXAMEN);
 		mav.addObject("listaPreguntas", evaluacionAlumnoService.listarPreguntasByEvaluacionAlumno(idEvaluacion)); //id examen, preguntas 
 		mav.addObject("listaRespuestas", evaluacionAlumnoService.listarRespuestas());
@@ -49,7 +63,7 @@ public class EvaluacionAlumnoController {
 		model.addAttribute("respuestasModel", respuestasModel);
 		model.addAttribute("preModel", preModel);
 		model.addAttribute("idEvaluacion", idEvaluacion);
-		model.addAttribute("eTiempo",evaluacionAlumnoService.tiempoExamen(3));
+		model.addAttribute("eTiempo",evaluacionAlumnoService.tiempoExamen(idEvaluacion));
 
 		return mav;
 	}
